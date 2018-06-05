a67831ab9b0c30a480ce0b76b0f7e4916e19ca17
CorpOnThron <koterator@gmail.com>
2018-06-05 07:43:40 -0400

    Ver. 1.00: Frequency done.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,98 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp4
 {
-    class Transaction
-    {
-        Priority priority;
-        string category;
-        string name;
-        double amount;
-        Frequency frequency;
-        List<DateTime> dates;
-        
-        public Transaction(double amount, Priority priority = Priority.None, Frequency frequency = Frequency.Once)
-        {
-            this.amount = amount;
-            this.priority = priority;
-            this.frequency = frequency;
-            dates.Add(DateTime.Now);
-            // we need to implement AutoNames
-            //switch statement to be implemented to define dates
-           
-        }
-
-    }
-
-    class User
-    {
-        double Balance;
-        List<Transaction> transaction;
-        //schedule if needed
-        double minimumBalance;
-        List<Income> income; //need to think about it
-        public User (double Balance = 0, double minimumBalance = 0)
-        {
-
-        }
-
-        public DateTime hitMinimumBalance()
-        {
-            //calculate the date when you hit the minimumBalance
-            return DateTime.Now;
-        }
-
-        public double avoidHittingMinumumBalance()
-        {
-            //calculate how much user needs to save this month to avoid hitting minumum balance
-            return 0;
-        }
-
-        public void pathToAvoid()
-        {
-            //method suggests the ways to avoid hitting the minimum balance
-            //think about it later
-        }
-    }
-
-    class Income
-    {
-        string NameIncome;
-        Frequency IncomeFrequency;
-        List<DateTime> IncomeDates;
-        double Amount;
-
-        public Income(string name, double amount, Frequency frequency = Frequency.Once)
-        {
-
-            this.NameIncome = name;
-            this.Amount = amount;
-            this.IncomeFrequency= frequency;
-            IncomeDates.Add(DateTime.Now);
-            // we need to implement AutoNames
-            //switch statement to be implemented to define dates
-
-        }
-    }
-
-    enum Frequency
+    //Enumerators
+    enum Freq
     {
         Daily,
-        Weekly,
-        Monthly,
-        Yearly,
-        Once
+    Weekly,
+    BiWeekly,
+    Monthly,
+    Yearly
     };
-    enum Priority
+
+    enum Position
     {
-        Unavoidable,
-        High,
-        Medium,
-        Low,
-        None
+        First,
+    Second,
+    Third,
+    Fourth,
+    Fifth
     };
+
+enum DayTypes
+{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Day,
+    Weekday,
+    WeekendDay
+};
+    [Flags]
+enum DaysOfWeek
+{
+    Sunday = 0,
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 4,
+    Thursday = 8,
+    Friday = 16,
+    Saturday = 32
+};
+    [Flags]
+enum Month
+{
+    January = 0,
+    February = 1,
+    March = 2,
+    April = 4,
+    May = 8,
+    June = 16,
+    July = 32,
+    August = 64,
+    September = 128,
+    October = 256,
+    November = 512,
+    December = 1024
+};
+
+
+    class Frequency
+{
+        // variables
+        Freq freq;
+    DateTime startDate;
+    DaysOfWeek daysOfWeek;
+    int x;
+    int[] DaysOfMonth;
+
+// Constructors
+
+Frequency(Freq Freq, DateTime startDate)    //This constructor sets the Freq variable of the class, and the startDate
+    {
+            this.freq = Freq;
+            this.startDate = startDate;
+    }
+Frequency(int x = 1)    //This constructor serts the Freq Variable of the class, and the enum DaysOfWeek. X lets the user skip the period by X amount
+    {
+            freq = Freq.Daily; 
+            this.x = x;
+    }
+Frequency(int x = 1,  Enum DaysOfWeek)
+        {
+            freq = Freq.Weekly;
+            this.x = x;
+        }
+Frequency(int X = 1, Params int DaysOfMonth)   ////This constructor sets the Freq Variable of the class, and the int array DaysOfMonth. X lets the user skip the period by X amount
+    {
+            freq = Freq.Monthly;
+        }
+Frequency(int X = 1, Month Month, Position Position, DayTypes DayTypes)
+    {
+            freq = Freq.Yearly;
+    }
+
+        public List<DateTime> GetNextDate()// return x number of dates, which satisfy frequency and starts from start date(by default today)
+        {
+            List<DateTime> shedule = new List<DateTime>();
+
+
+            return shedule;
+        }
+    }
 }
