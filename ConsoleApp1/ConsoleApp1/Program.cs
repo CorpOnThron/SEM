dc0c39aed1bb1ac56430226bedde7cc9eea0fbd5
CorpOnThron <koterator@gmail.com>
2018-06-06 12:35:25 -0400

    Ver 1.0.1: Shedule Prototype done.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -75,7 +75,7 @@ enum Month
 
 // Constructors
 
-Frequency(Freq Freq, DateTime startDate)    //This constructor sets the Freq variable of the class, and the startDate
+Frequency(Freq Freq, DateTime startDate = DateTime.Now)    //This constructor sets the Freq variable of the class, and the startDate  CHECK OUT CHANGES!!!
     {
             this.freq = Freq;
             this.startDate = startDate;
@@ -90,7 +90,7 @@ Frequency(int x = 1,  Enum DaysOfWeek)
             freq = Freq.Weekly;
             this.x = x;
         }
-Frequency(int X = 1, Params int DaysOfMonth)   ////This constructor sets the Freq Variable of the class, and the int array DaysOfMonth. X lets the user skip the period by X amount
+Frequency(int X = 1, Params int DaysOfMonth)   //This constructor sets the Freq Variable of the class, and the int array DaysOfMonth. X lets the user skip the period by X amount
     {
             freq = Freq.Monthly;
         }
@@ -99,10 +99,61 @@ Frequency(int X = 1, Month Month, Position Position, DayTypes DayTypes)
             freq = Freq.Yearly;
     }
 
-        public List<DateTime> GetNextDate()// return x number of dates, which satisfy frequency and starts from start date(by default today)
+        public List<DateTime> GetNextDate(Frequency freqObj, int ah = 10)// return x number of dates, which satisfy frequency and starts from start date(by default today)
         {
             List<DateTime> shedule = new List<DateTime>();
 
+            switch (freqObj)
+            {
+                case Daily:
+                    DateTime date1 = freqObj.startDate;
+                    int i = 1;
+                    shedule[0] = freqObj.startDate;
+                    while (i < ah)
+                    {
+                        double da = Convert.ToDouble(freqObj.x);
+                        shedule[i] = date1.AddDays(da);
+                        i++;
+                    }
+                    break;
+                case Weekly:
+                    DateTime date1 = freqObj.startDate;
+                    int i = 0;
+                    shedule[0] = freqObj.startDate;
+                    while (i < ah)
+                    {
+                        double da = Convert.ToDouble(freqObj.x) * 7;
+                        shedule[i] = date1.AddDays(da);
+                        i++;
+                    }
+                    break;
+                case BiWeekly:// what is this?
+                    break;
+                case Monthly:
+                    DateTime date1 = freqObj.startDate;
+                    int i = 0;
+                    shedule[0] = freqObj.startDate;
+                    while (i < ah)
+                    {
+                        double da = Convert.ToDouble(freqObj.x);
+                        shedule[i] = date1.AddMonths(da);
+                        i++;
+                    }
+                    break;
+                case Yearly:
+                    DateTime date1 = freqObj.startDate;
+                    int i = 0;
+                    shedule[0] = freqObj.startDate;
+                    while (i < ah)
+                    {
+                        double da = Convert.ToDouble(freqObj.x);
+                        shedule[i] = date1.AddYears(da);
+                        i++;
+                    }
+                    break;
+                default:
+                    break;
+            }
 
             return shedule;
         }
